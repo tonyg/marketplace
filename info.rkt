730f154af2ff9f44b6bdd06dc28e723b54e2409c
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-05-12 10:58:46 -0400

    Deps. Closes #9, perhaps

--- info.rkt
+++ info.rkt
@@ -1,2 +1,11 @@
 #lang setup/infotab
 (define scribblings '(("scribblings/marketplace.scrbl" (multi-page))))
+(define deps '("base"
+               "data-lib"
+               "gui-lib"
+               "images-lib"
+               "rackunit-lib"
+               "web-server-lib"))
+(define build-deps '("scribble-lib"
+                     "slideshow-lib"
+                     "typed-racket-lib"))
